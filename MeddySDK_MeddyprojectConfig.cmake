bdf5851b3882ee842929f931f800ad19df84169b
Christian Hinkle <christianhinko@gmail.com>
2024-02-18 07:54:17 -0500

    [CMake] Add find_dependency() call to config script that should've been there.

--- MeddySDK_MeddyprojectConfig.cmake
+++ MeddySDK_MeddyprojectConfig.cmake
@@ -1,3 +1,8 @@
 
+include(CMakeFindDependencyMacro)
+
+# Get the Boost export.
+find_dependency(Boost CONFIG COMPONENTS filesystem)
+
 # Include our export. This imports all of our targets.
 include("${CMAKE_CURRENT_LIST_DIR}/MeddySDK_MeddyprojectExport.cmake")
