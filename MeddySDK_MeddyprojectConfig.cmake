f322f26673d9215e58d57f761da0a078c2558f85
Christian Hinkle <christianhinko@gmail.com>
2024-03-24 05:53:56 -0400

    [CMake] Change alias target convention. Update documentation.
    Renamed from "::MeddySDK_Meddyproject" to "MeddySDK::Meddyproject". This requires custom alias setup in the export's config file.
    
    Set up aliases in the export's config file, with documentation.

--- MeddySDK_MeddyprojectConfig.cmake
+++ MeddySDK_MeddyprojectConfig.cmake
@@ -6,3 +6,12 @@ find_dependency(Boost CONFIG COMPONENTS filesystem)
 
 # Include our export. This imports all of our targets.
 include("${CMAKE_CURRENT_LIST_DIR}/MeddySDK_MeddyprojectExport.cmake")
+
+#
+# Add alias targets whose names match those from the project file.
+#
+# It's important to make sure the names are the same for consistency. Namely, so that build
+# interface users can use `OVERRIDE_FIND_PACKAGE` with `FetchContent_Declare()`.
+#
+
+add_library(MeddySDK::Meddyproject ALIAS MeddySDK_Meddyproject)
