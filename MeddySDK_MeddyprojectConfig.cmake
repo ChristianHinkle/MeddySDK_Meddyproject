60be3e6b31ede73f96e076eed936851f7ed0ca4d
Christian Hinkle <christianhinko@gmail.com>
2025-08-12 16:34:13 -0600

    [CMake] Delete lingering usage of Boost Filesystem from our package configuration file. This should've been deleted in commit d29a878b970a7af7fa3f9026d61bdb38b5e47b8a, but it was forgotten.

--- MeddySDK_MeddyprojectConfig.cmake
+++ MeddySDK_MeddyprojectConfig.cmake
@@ -2,9 +2,6 @@
 
 include(CMakeFindDependencyMacro)
 
-# Get the Boost export.
-find_dependency(Boost CONFIG COMPONENTS filesystem)
-
 # Include our export. This imports all of our targets.
 include("${CMAKE_CURRENT_LIST_DIR}/MeddySDK_MeddyprojectExport.cmake")
 
