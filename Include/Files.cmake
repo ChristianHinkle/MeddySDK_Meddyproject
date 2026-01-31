9fcf8a7fbd1cb1ee7884fcc6019227879f05fae7
Christian Hinkle <christianhinko@gmail.com>
2026-01-30 17:37:52 -0700

    [cmake][files][cpp] Copy cmake file list improvements from CppUtils_Core: Move file lists into their own "Files.cmake" modules and include them where the cmake code uses them. So now, code writers don't have to dig through cmake code to list their source files that they add. Rename redundant "Include" and "Source" directories to "Files" as they purely hold the source/include files and correspond with the contents of the "Files.cmake".

---
+++ Include/Files.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
+
+# Set this variable for the person including us to use.
+set(FILES_CMAKE_RESULT
+  # List out all our include files.
+  "${CMAKE_CURRENT_SOURCE_DIR}/Files/${MY_BASE_PROJECT_NAME_FULL}_Export.h"
+  "${CMAKE_CURRENT_SOURCE_DIR}/Files/${MY_BASE_PROJECT_NAME_FULL}_include_export.h"
+  "${CMAKE_CURRENT_SOURCE_DIR}/Files/${MY_BASE_PROJECT_NAME_FULL}/Meddyproject.h"
+  "${CMAKE_CURRENT_SOURCE_DIR}/Files/${MY_BASE_PROJECT_NAME_FULL}/FilesystemUtils.h"
+  )
---
+++ Source/Files.cmake
@@ -0,0 +1,9 @@
+# Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
+
+# Set this variable for the person including us to use.
+set(FILES_CMAKE_RESULT
+  # List out all our source files, relative to our "Files" directory.
+  "${MY_BASE_PROJECT_NAME_FULL}_Export.cpp"
+  "Meddyproject.cpp"
+  "FilesystemUtils.cpp"
+  )
